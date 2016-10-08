5822fd1b41acb6f1e21d95fb73af554f4a7ba417
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-10-08 20:53:16 -0300

    Initial commit, starter files and imports

---
+++ src/app.js
@@ -0,0 +1,24 @@
+(function () {
+
+angular.module('MenuApp',['ui.router']);
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'menu.html'
+    })
+
+}
+
+
+})();
