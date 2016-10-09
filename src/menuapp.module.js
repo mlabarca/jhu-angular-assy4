066b45a33c48694d4379d54413fe33f8a8ac9cf2
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-10-09 12:59:53 -0300

    Add loading spinner component from lecture

--- src/menuapp.module.js
+++ src/menuapp.module.js
@@ -1,6 +1,6 @@
 (function () {
 'use strict'
-angular.module('MenuApp',['ui.router', 'data']);
+angular.module('MenuApp',['ui.router', 'data', 'Spinner']);
 
 angular.module('MenuApp')
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
