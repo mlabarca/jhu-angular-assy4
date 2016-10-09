2af291aee0aa6f94f682851f7a01ec367d8733ad
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-10-08 21:25:11 -0300

    Add data module, menudata service

--- src/menu/menudata.service.js
+++ src/menu/menudata.service.js
@@ -2,5 +2,32 @@
 'use strict'
 
 angular.module('data')
-.service('MenuData');
+.service('MenuData', MenuDataService);
+
+MenuDataService.$inject = ['$http', 'ApiBasePath'];
+function MenuDataService($http, ApiBasePath){
+  var service = this;
+  service.getAllCategories = function (searchTerm){
+      var httpParams = {
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json")
+      };
+
+      return $http(httpParams).then(function (response) {
+        var menuItems = response.data.menu_items;
+
+        // process result and only keep items that match
+        var foundItems = [];
+        for(var index = 0; index < menuItems.length; index++){
+          if(menuItems[index].description.indexOf(searchTerm) != -1){
+            foundItems.push(menuItems[index]);
+          }
+        }
+
+        // return processed items
+        return foundItems;
+      });
+    });
+  };
+}
 })();
---
+++ src/menuapp.module.js
@@ -0,0 +1,24 @@
+(function () {
+'use strict'
+angular.module('MenuApp',['ui.router', 'data']);
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'menu.html'
+    })
+
+}
+
+
+})();
