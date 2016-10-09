ee05a709d72cc6664bcfa3808414f5553aa4f7ca
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-10-09 12:42:07 -0300

    Add items route state, controller, template

--- src/routes.js
+++ src/routes.js
@@ -29,6 +29,17 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
       }
     })
 
+    .state('items', {
+      url: '/items/{categoryShortName}',
+      templateUrl: 'src/items/itemsList.template.html',
+      controller: 'ItemsController as itemsCtrl',
+      resolve: {
+        items: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService){
+          return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+        }]
+      }
+    })
+
 }
 
 
